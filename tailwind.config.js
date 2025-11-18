f1f62c3e355b52a9443bc4bcb10e5344f4a7058b
sejin <ks320205@naver.com>
2025-11-18 09:49:27 +0900

    chore: tailwind 셋팅

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Vite 가 사용하는 Html
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
