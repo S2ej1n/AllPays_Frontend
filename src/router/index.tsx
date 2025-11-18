34752f19fa9be7ab4ea9336308ffa2033afd55bd
sejin <ks320205@naver.com>
2025-11-18 22:54:47 +0900

    chore: 리액트 라우터 기본 설정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,1 +1,22 @@
 // 라우터 기본 설정
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "../App"
+
+const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <App />,
+            children: [
+                {
+                    // 하위 페이지 추가 작성
+                }
+            ]
+
+        }
+]);
+
+const AppRouter = () => {
+    return <RouterProvider router={router} />
+};
+
+export default AppRouter;
