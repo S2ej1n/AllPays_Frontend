270a968e01396a161a711c9b1461c7bf11eda718
sejin <ks320205@naver.com>
2025-11-20 18:38:42 +0900

    feat: 공통 커스텀 훅 get 제작

--- src/apis/hook.ts
+++ src/apis/hook.ts
@@ -1,1 +1,18 @@
 // Tanstack Query를 이용한 커스텀 훅
+import { useQuery } from '@tanstack/react-query';
+import axiosInstance from "./axios.ts";
+import type {ResponseType} from "./axios.ts"
+
+// GET 호출 훅 제작
+export const useGethook = <T>(path: string) => {
+
+    const fetcher = async () : Promise<T> => {
+        const res = await axiosInstance.get<ResponseType<T>>(path);
+        return res.data.data;
+    };
+
+    return useQuery({
+        queryKey: ["api", path],
+        queryFn: fetcher,
+    });
+}
--- src/apis/payment.ts
+++ src/apis/payment.ts
@@ -1,6 +1,5 @@
 // [거래 내역 전체 조회] /payments/list 거래 내역 요청
 
-import axiosInstance from "./axios.ts";
 import type { ResponseType} from "./axios.ts";
 import type { Payment } from '../types/payments'
 
