86bf9dd3d8ec191ea13a50593c64c2cdb4ce5c46
sejin <ks320205@naver.com>
2025-11-21 06:42:52 +0900

    feat: 필터링 요소 클릭시 색상 변경

--- src/pages/DashBoard/com/PeriodFilter.tsx
+++ src/pages/DashBoard/com/PeriodFilter.tsx
@@ -1,17 +1,31 @@
 // 기간 필터링 컴포넌트
+import { useFilterStore } from "../../../store/filterStore"
+
 export default function PeriodFilter() {
+    // zustand 사용
+    const { period, setPeriod } = useFilterStore();
+
+    // 참 : 그짓
+    const buttonClick = (click: boolean) => { return click 
+        ? "btn btn-sm p-4 px-3 border border-blue-200 shadow-sm text-sm rounded-md bg-blue-100 text-blue-600 "
+        : "btn btn-sm p-4 px-3 border border-gray-300 shadow-sm text-sm rounded-md text-gray-500"
+    }
+
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4 p-4 px-5 bg-white border border-gray-300 shadow-sm rounded-2xl">
         <div>
             <p className="font-bold mb-2">기간 선택</p>
             <div className="flex flex-row gap-2">
-            <button className="btn btn-sm p-4 px-3 border border-gray-300 shadow-sm text-sm rounded-md text-gray-500">
+            <button className={buttonClick(period === "WEEK")}
+                onClick={() => setPeriod("WEEK")}>
                 이번주
             </button>
-            <button className="btn btn-sm p-4 px-3 border border-gray-300 shadow-sm text-sm rounded-md text-gray-500">
+            <button className={buttonClick(period === "MONTH")}
+                onClick={() => setPeriod("MONTH")}>
                 월별
             </button>
-            <button className="btn btn-sm p-4 px-3 border border-gray-300 shadow-sm text-sm rounded-md text-gray-500">
+            <button className={buttonClick(period === "YEAR")}
+                onClick={() => setPeriod("YEAR")}>
                 연도별
             </button>
             </div>
--- src/store/filterStore.ts
+++ src/store/filterStore.ts
@@ -1,5 +1,6 @@
 // 필터 상태 관리하는 store
 // 주별, 월별, 년도별
+// 필터링 하나에 컴포넌트의 모든 요소 값이 바뀌어야하므로 zustand로 전역관리 하기로 했습니다.
 
 import { create } from "zustand";
 export type PeriodType = "WEEK" | "MONTH" | "YEAR";
