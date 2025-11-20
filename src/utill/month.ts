d326aca2c45d69e8863d62ed473d1d1adbe0cc19
sejin <ks320205@naver.com>
2025-11-21 04:19:52 +0900

    feat: 월별 필터링 함수

--- src/utill/month.ts
+++ src/utill/month.ts
@@ -1,7 +1,20 @@
 // 월별로 필터링 하는 함수
+// {x:월, y:토탈 값}
 // "paymentAt": "2025-11-01T00:10:00"
+// "amount": "11000.00" -> 스트링 타입
+
 import type { Payment } from "../types/payments";
 
 export function filterMonth(data: Payment[]){
+    // 유사 배열 객체 
+    // Array.from({length: 5}, (v, i) => i);
+    const result = Array.from({length:12},(v, i)=>({month: i + 1, total:0}));
 
+    // for문 돌려
+    data.forEach((i) => {
+        // Date 객체로 만들기 (string으로 하면 일일이 분리하지만, Date객체로 만들면 분리하기 easy합니다)
+        const date = new Date(i.paymentAt);
+        const month = date.getMonth();
+        result[month].total += Number(i.amount);
+    });
 }
