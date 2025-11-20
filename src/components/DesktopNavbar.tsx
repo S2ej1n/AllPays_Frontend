99a8eb8ade5a67d5e0b0bd260de9131f3933f951
sejin <ks320205@naver.com>
2025-11-20 19:35:24 +0900

    design: 스타일 추가

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -2,8 +2,9 @@
 
 export default function DesktopNavbar() {
     return (
-        <aside className="hidden md:flex flex-col w-60 bg-gray-200 p-4 gap-4">
-            <div className="p-2 font-bold">메인 logo</div>
+        <aside className="hidden md:flex flex-col w-60 bg-white p-4 gap-4
+        border-r border-gray-300">
+            <div className="p-2 font-bold">Pays</div>
             <button className="bg-white p-2 text-left">대시보드</button>
             <button className="bg-white p-2 text-left">가맹점조회</button>
             <button className="bg-white p-2 text-left">메뉴</button>
